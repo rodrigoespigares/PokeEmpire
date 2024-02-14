055ac3347f576a34799cf51ade59efbb0d242c10
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-02-14 12:17:24 +0100

    juego cambios

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './Play.css'
 import { text } from './text'
+    
+
 
 
 export default function Play() {
-    let img;
-    let [btn, setBtn] = useState([]);
+    let [img, setImg] =useState("");
+    let [nombres, setNombres] = useState([]);
     let [todo, setTodo] = useState("");
-    let win = ""
+    let [win,setWin] = useState("")
+    let [winId,setWinId] = useState(0);
     function jugar(e) {
         if (e.target.id) {
             e.target.hidden = true;
@@ -15,33 +18,58 @@ export default function Play() {
         fetch('https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 1000 + 1))
             .then((response) => response.json())
             .then((pokemonData) => {
-                img = <img key={pokemonData.id} className='imagen__juego' src={pokemonData.sprites?.other['official-artwork'].front_default}></img>
-                setBtn(...btn,<button id={pokemonData.name} key={pokemonData.name} className='text-black' onClick={opciones}>{pokemonData.name}</button>);
-                setBtn(...btn,<button id={pokemonData.name} key={pokemonData.name} className='text-black' onClick={opciones}>{pokemonData.name}</button>);
-                win = pokemonData.name;
+                setImg(<img key={pokemonData.id} className='imagen__juego' src={pokemonData.sprites?.other['official-artwork'].front_default}></img>);
+                setNombres([...nombres,pokemonData.name]);
+                setWin(pokemonData.name);
+                setWinId(pokemonData.id);
             }
         );
+        
+        let aleatorio = Math.floor(Math.random() * 1000 + 1);
+        while(winId == aleatorio){
+            aleatorio = Math.floor(Math.random() * 1000 + 1)
+        }
+        rellenar(aleatorio);
     };
-    console.log(btn)
+    console.log(nombres);
+    function rellenar(num) {
+
+        fetch('https://pokeapi.co/api/v2/pokemon/' + num)
+            .then((response) => response.json())
+            .then((pokemonData) => {
+                setNombres([...nombres,pokemonData.name]);
+            }
+        );
+        
+        
+    };
     function opciones(e) {
+        console.log(win)
         console.log(e.target.id == win);
     }
-    if(btn.length==4){
+    if(nombres.length==1){
         setTodo(
             <div className='w-50 d-flex justify-content-center align-items-center'>
                 {img}
                 <div className='d-flex flex-column'>
-                    {btn}
+                    <button id={nombres} onClick={opciones}>{nombres}</button>
                 </div>
             </div>
         )
+        setNombres([]);
+    }else if(nombres.length>1){
+        let aleatorio = Math.floor(Math.random() * 1000 + 1);
+        while(winId == aleatorio){
+            aleatorio = Math.floor(Math.random() * 1000 + 1)
+        }
+        rellenar(aleatorio);
     }
 
     return (
         <div className='d-flex justify-content-center align-items-center h-100 flex-column'>
             <h2 className='display-1'>How its that pokemon?</h2>
             {todo}
-            <button className='text-white btn fs-1 jugar' id='jugar' onClick={jugar}>PLAY</button>
+            <button className='text-white nombres fs-1 jugar' id='jugar' onClick={jugar}>PLAY</button>
         </div>
     )
 }
