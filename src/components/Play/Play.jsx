19c1044b8dfe4fa5acaea43580e6ffcebfdb3f03
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-02-14 21:42:17 +0100

    Emezando el juego de cero

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -1,75 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import './Play.css'
-import { text } from './text'
-    
-
-
+import React, { useState, useEffect } from 'react';
 
 export default function Play() {
-    let [img, setImg] =useState("");
-    let [nombres, setNombres] = useState([]);
-    let [todo, setTodo] = useState("");
-    let [win,setWin] = useState("")
-    let [winId,setWinId] = useState(0);
-    function jugar(e) {
-        if (e.target.id) {
-            e.target.hidden = true;
-        }
-        fetch('https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 1000 + 1))
-            .then((response) => response.json())
-            .then((pokemonData) => {
-                setImg(<img key={pokemonData.id} className='imagen__juego' src={pokemonData.sprites?.other['official-artwork'].front_default}></img>);
-                setNombres([...nombres,pokemonData.name]);
-                setWin(pokemonData.name);
-                setWinId(pokemonData.id);
-            }
-        );
-        
-        let aleatorio = Math.floor(Math.random() * 1000 + 1);
-        while(winId == aleatorio){
-            aleatorio = Math.floor(Math.random() * 1000 + 1)
-        }
-        rellenar(aleatorio);
-    };
-    console.log(nombres);
-    function rellenar(num) {
+    const [img, setImg] = useState("");
+    const [options, setOptions] = useState([]);
+    const [correctPokemon, setCorrectPokemon] = useState("");
 
-        fetch('https://pokeapi.co/api/v2/pokemon/' + num)
-            .then((response) => response.json())
-            .then((pokemonData) => {
-                setNombres([...nombres,pokemonData.name]);
-            }
-        );
-        
-        
-    };
-    function opciones(e) {
-        console.log(win)
-        console.log(e.target.id == win);
+    useEffect(() => {
+        iniciarJuego();
+    }, []);
+
+    function iniciarJuego() {
+        const aleatorio = Math.floor(Math.random() * 1000);
+        fetchPokemon(aleatorio + 1);
     }
-    if(nombres.length==1){
-        setTodo(
-            <div className='w-50 d-flex justify-content-center align-items-center'>
-                {img}
-                <div className='d-flex flex-column'>
-                    <button id={nombres} onClick={opciones}>{nombres}</button>
-                </div>
-            </div>
-        )
-        setNombres([]);
-    }else if(nombres.length>1){
-        let aleatorio = Math.floor(Math.random() * 1000 + 1);
-        while(winId == aleatorio){
-            aleatorio = Math.floor(Math.random() * 1000 + 1)
+
+    function fetchPokemon(id) {
+        fetch('https://pokeapi.co/api/v2/pokemon/' + id)
+            .then(response => response.json())
+            .then(pokemonData => {
+                setCorrectPokemon(pokemonData.name);
+                setImg(<img key={pokemonData.id} className='imagen__juego' src={pokemonData.sprites?.other['official-artwork'].front_default} alt={pokemonData.name} />);
+                generarOpciones(pokemonData.name);
+            });
+    }
+
+    function generarOpciones(correctName) {
+        const opciones = [];
+        for (let i = 0; i < 4; i++) {
+            const aleatorio = Math.floor(Math.random() * 1000 + 1);
+
+            fetch('https://pokeapi.co/api/v2/pokemon/' + aleatorio)
+                .then(response => response.json())
+                .then(pokemonData => {
+                    opciones.push(pokemonData.name);
+                    if (opciones.length === 4) {
+                        const respuestaCorrecta = Math.floor(Math.random() * 4);
+                        opciones[respuestaCorrecta] = correctName;
+                        setOptions(opciones);
+                    }
+                });
         }
-        rellenar(aleatorio);
+    }
+
+    function opciones(e) {
+        const selectedPokemon = e.target.innerText;
+        const isCorrect = selectedPokemon === correctPokemon;
+        console.log(isCorrect ? "¡Correcto!" : "Incorrecto");
     }
 
     return (
         <div className='d-flex justify-content-center align-items-center h-100 flex-column'>
-            <h2 className='display-1'>How its that pokemon?</h2>
-            {todo}
-            <button className='text-white nombres fs-1 jugar' id='jugar' onClick={jugar}>PLAY</button>
+            <h2 className='display-1'>How its that Pokemon</h2>
+            {img}
+            <div className='d-flex flex-column'>
+                {options.map((option, index) => (
+                    <button key={index} onClick={opciones} className='text-white nombres fs-1 jugar'>{option}</button>
+                ))}
+            </div>
+            <button className='text-white nombres fs-1 jugar' id='jugar' onClick={iniciarJuego}>PLAY</button>
         </div>
-    )
+    );
 }
