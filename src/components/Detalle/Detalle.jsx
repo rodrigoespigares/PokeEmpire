8f95ccbccafefd1fe474fd1f3f1df8ede4a849d6
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-02-16 14:08:57 +0100

    avances en el detalle

--- src/components/Detalle/Detalle.jsx
+++ src/components/Detalle/Detalle.jsx
@@ -1,60 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './Detalle.css';
-
+import StatsChart from '../StatsChart/StatsChart';
 
 export default function Detalle() {
-  let { id } = useParams();
-  let [pokemon, setPokemon] = useState("");
-  let [click, setClick] = useState(0);
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState("");
+  const [click, setClick] = useState(0);
+  const [stats, setStats] = useState([]);
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/' + id)
       .then((response) => response.json())
       .then((pokemonData) => {
         setPokemon(pokemonData);
-
+        setStats(pokemonData.stats);
       });
-  }, []);
-
-  function changeImg(e){
-    setClick(parseInt(e.target.id))
+  }, [id]);
+  function changeImg(e) {
+    setClick(parseInt(e.target.id));
   }
 
   let images = [];
-  let key= 0;
-  let habilidades = [] 
-  let tipo = []
-  let stats = [];
-  if (pokemon != "") {
-    pokemon.stats.forEach(element =>{
-        stats.push( <p key={element.stat.name}>{element.stat.name}: {element.base_stat}</p>);
-    })
-    pokemon.types.forEach(element => {
-      tipo.push(<article key={element.type.name}>{element.type.name}</article>)
-    })
-    pokemon.abilities.forEach(element => {
+  let key = 0;
+  let habilidades = [];
+  let tipo = [];
+  if (pokemon !== "") {
+    pokemon.types.forEach((element) => {
+      tipo.push(<article key={element.type.name}>{element.type.name}</article>);
+    });
+    pokemon.abilities.forEach((element) => {
       habilidades.push(<article key={element.ability.name}>{element.ability.name}</article>);
     });
     Object.keys(pokemon.sprites.other).map((element) =>
-        Object.keys(pokemon.sprites.other[element]).map(into => {
-          if (pokemon.sprites.other[element][into] != null) {
-            images.push(<div key={pokemon.sprites.other[element][into]} onClick={changeImg}><img key={pokemon.sprites.other[element][into]} id={key} src={pokemon.sprites.other[element][into]}></img></div>)
-            key++;
-          }
+      Object.keys(pokemon.sprites.other[element]).map((into) => {
+        if (pokemon.sprites.other[element][into] != null) {
+          images.push(
+            <div key={pokemon.sprites.other[element][into]} onClick={changeImg}>
+              <img key={pokemon.sprites.other[element][into]} id={key} src={pokemon.sprites.other[element][into]} alt="sprite"></img>
+            </div>
+          );
+          key++;
         }
-      )
-    )
+      })
+    );
   }
+
   return (
     <section id='detalle'>
       <div>
-        <div className='detalle__principal'>
-          {images[click]}
-        </div>
-        <div className='detalle__conjunto'>
-          {images}
-        </div>
+        <div className='detalle__principal'>{images[click]}</div>
+        <div className='detalle__conjunto'>{images}</div>
       </div>
       <div>
         <h2 className='display-1 text-uppercase'>{pokemon.name}</h2>
@@ -71,10 +67,9 @@ export default function Detalle() {
         </section>
         <section>
           <h3>Stats</h3>
-          {stats}
+          <StatsChart stats={pokemon.stats} />
         </section>
-
       </div>
     </section>
-  )
+  );
 }
---
+++ src/components/StatsChart/StatsChart.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const StatsChart = ({ stats }) => {
+  const chartRef = useRef();
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    if (!stats || stats.length === 0) {
+      return;
+    }
+    
+    const ctx = chartRef.current.getContext('2d');
+    const data = {
+      labels: stats.map((stat) => stat.stat.name.toUpperCase()),
+      datasets: [
+        {
+          data: stats.map((stat) => stat.base_stat),
+          backgroundColor: 'rgba(255,255,255,0.2)',
+          borderColor: 'rgba(255,255,255,1)',
+          borderWidth: 2,
+          pointBackgroundColor: 'rgba(255,255,255,1)',
+          pointBorderColor: 'rgba(255,255,255,1)',
+        },
+      ],
+    };
+
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
+
+    chartInstance.current = new Chart(ctx, {
+        type: 'radar',
+        data: data,
+        options: {
+          scales: {
+            r: {
+              angleLines: {
+                display: true,
+                color: 'rgba(255,255,255,0.6)', 
+              },
+              pointLabels: {
+                font: {
+                  color: 'rgba(0,0,0,1)', 
+                },
+              },
+              ticks: {
+                display: false, 
+              },
+              suggestedMin: 0,
+              suggestedMax: 100,
+            },
+          },
+          plugins: {
+            legend: {
+              display: false, 
+            },
+          },
+        },
+      });
+  }, [stats]);
+
+  return <canvas ref={chartRef} width="400" height="200"></canvas>;
+};
+
+export default StatsChart;
